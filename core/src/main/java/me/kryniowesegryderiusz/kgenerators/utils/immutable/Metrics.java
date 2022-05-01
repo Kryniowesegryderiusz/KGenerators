b4a89cecf66e2afc6df0dd8c9b95183d2d3ef86b
Kryniowesegryderiusz <mrkondziou@gmail.com>
2022-05-01 16:10:02 +0200

    Updated config comments

--- core/src/main/java/me/kryniowesegryderiusz/kgenerators/utils/immutable/Metrics.java
+++ core/src/main/java/me/kryniowesegryderiusz/kgenerators/utils/immutable/Metrics.java
@@ -137,7 +137,7 @@ public class Metrics {
         }
 
         // Load the data
-        enabled = config.getBoolean("enabled", true);
+        enabled = true;
         serverUUID = config.getString("serverUuid");
         logFailedRequests = config.getBoolean("logFailedRequests", false);
         logSentData = config.getBoolean("logSentData", false);
