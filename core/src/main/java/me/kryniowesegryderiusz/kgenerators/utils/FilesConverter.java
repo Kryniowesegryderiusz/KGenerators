29a2afc276befde9f03f582f6561aff5f2241a26
Kryniowesegryderiusz <mrkondziou@gmail.com>
2022-07-13 05:50:44 +0200

    Fixed block-drop-to-eq files converter

--- core/src/main/java/me/kryniowesegryderiusz/kgenerators/utils/FilesConverter.java
+++ core/src/main/java/me/kryniowesegryderiusz/kgenerators/utils/FilesConverter.java
@@ -85,11 +85,11 @@ public class FilesConverter {
 			Logger.info("FilesConverter: Added pick-up-to-eq settings to config file");
 		}
 		
-		if (!config.contains("drop-to-eq"))
+		if (!config.contains("block-drop-to-eq"))
 		{
 			add(file, "");
 			add(file, "#Should block-related drops be added directly to inventory?");
-			add(file, "drop-to-eq: false");
+			add(file, "block-drop-to-eq: false");
 			Logger.info("FilesConverter: Added drop-to-eq settings to config file");
 		}
 		
