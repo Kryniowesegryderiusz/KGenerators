8d5fc78fa70fda673d9cb372766fd368930e1c93
Kryniowesegryderiusz <mrkondziou@gmail.com>
2022-10-30 17:08:13 +0100

    NBTAPI Update and getKeys fix

--- core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/NBTAPIHook.java
+++ core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/NBTAPIHook.java
@@ -35,17 +35,31 @@ public class NBTAPIHook {
 		return item;
 	}
 	
+	public static ItemStack addNBT(ItemStack item, String place, String key, double value) {
+		
+		try {
+			NBTItem nbti = new NBTItem(item);
+			nbti.setDouble(key, value);
+			nbti.applyNBT(item);
+		} catch (Exception e) {
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to " + item.toString());
+			Logger.error(e);
+		}
+		
+		return item;
+	}
+	
 	public static String getNBTString(ItemStack item, String place, String key) {
 		
 		try {
 			NBTItem nbti = new NBTItem(item);
 			
-			if (nbti.getKeys().contains(key))
+			if (nbti.hasKey(key))
 				return nbti.getString(key);
 			else return null;
 			
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT " + key + " from " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
 			Logger.error(e);
 		}
 		
@@ -57,12 +71,29 @@ public class NBTAPIHook {
 		try {
 			NBTItem nbti = new NBTItem(item);
 			
-			if (nbti.getKeys().contains(key))
+			if (nbti.hasKey(key))
 				return nbti.getInteger(key);
 			else return null;
 			
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT " + key + " from " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
+			Logger.error(e);
+		}
+		
+		return null;
+	}
+	
+	public static Double getNBTDouble(ItemStack item, String place, String key) {
+		
+		try {
+			NBTItem nbti = new NBTItem(item);
+			
+			if (nbti.hasKey(key))
+				return nbti.getDouble(key);
+			else return null;
+			
+		} catch (Exception e) {
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
 			Logger.error(e);
 		}
 		
