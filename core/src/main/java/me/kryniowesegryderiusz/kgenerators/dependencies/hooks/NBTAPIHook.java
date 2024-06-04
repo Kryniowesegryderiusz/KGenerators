c3b80964537231bd3e4f875b4bf1d983b62b582d
Konrad Boś <mrkondzioU@gmail.com>
2024-06-05 01:44:17 +0200

    NBTAPI update

--- core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/NBTAPIHook.java
+++ core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/NBTAPIHook.java
@@ -2,101 +2,99 @@ package me.kryniowesegryderiusz.kgenerators.dependencies.hooks;
 
 import org.bukkit.inventory.ItemStack;
 
-import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.changeme.nbtapi.NBT;
 import me.kryniowesegryderiusz.kgenerators.logger.Logger;
 
 public class NBTAPIHook {
-	
+
 	public static ItemStack addNBT(ItemStack item, String place, String key, String value) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			nbti.setString(key, value);
-			nbti.applyNBT(item);
+			NBT.modify(item, nbt -> {
+				nbt.setString(key, value);
+			});
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return item;
 	}
-	
+
 	public static ItemStack addNBT(ItemStack item, String place, String key, int value) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			nbti.setInteger(key, value);
-			nbti.applyNBT(item);
+			NBT.modify(item, nbt -> {
+				nbt.setInteger(key, value);
+			});
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return item;
 	}
-	
+
 	public static ItemStack addNBT(ItemStack item, String place, String key, double value) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			nbti.setDouble(key, value);
-			nbti.applyNBT(item);
+			NBT.modify(item, nbt -> {
+				nbt.setDouble(key, value);
+			});
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot apply NBT " + key + ":" + value + " to "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return item;
 	}
-	
+
 	public static String getNBTString(ItemStack item, String place, String key) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			
-			if (nbti.hasTag(key))
-				return nbti.getString(key);
-			else return null;
-			
+			return NBT.get(item, nbt -> {
+				return nbt.getString(key);
+			});
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return null;
 	}
-	
+
 	public static Integer getNBTInteger(ItemStack item, String place, String key) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			
-			if (nbti.hasTag(key))
-				return nbti.getInteger(key);
-			else return null;
-			
+			return NBT.get(item, nbt -> {
+				return nbt.getInteger(key);
+			});
+
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return null;
 	}
-	
+
 	public static Double getNBTDouble(ItemStack item, String place, String key) {
-		
+
 		try {
-			NBTItem nbti = new NBTItem(item);
-			
-			if (nbti.hasTag(key))
-				return nbti.getDouble(key);
-			else return null;
-			
+			return NBT.get(item, nbt -> {
+				return nbt.getDouble(key);
+			});
 		} catch (Exception e) {
-			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from " + item.toString());
+			Logger.error("Dependencies: NBTAPIHook: " + place + ": Cannot get NBT String " + key + " from "
+					+ item.toString());
 			Logger.error(e);
 		}
-		
+
 		return null;
 	}
 
