fc495fd3845dfa07c93f766440e9e263960c07c7
Konrad Boś <mrkondzioU@gmail.com>
2024-09-24 22:25:49 +0200

    Updated IridiumSkyblock hook to newest API

--- core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/IridiumSkyblockHook.java
+++ core/src/main/java/me/kryniowesegryderiusz/kgenerators/dependencies/hooks/IridiumSkyblockHook.java
@@ -7,7 +7,6 @@ import org.bukkit.event.Listener;
 
 import com.iridium.iridiumskyblock.api.IridiumSkyblockAPI;
 import com.iridium.iridiumskyblock.api.IslandDeleteEvent;
-import com.iridium.iridiumskyblock.api.IslandRegenEvent;
 import com.iridium.iridiumskyblock.database.Island;
 
 import me.kryniowesegryderiusz.kgenerators.Main;
@@ -18,30 +17,26 @@ public class IridiumSkyblockHook implements Listener {
 	public static void setup() {
 		Main.getInstance().getServer().getPluginManager().registerEvents(new IridiumSkyblockHook(), Main.getInstance());
 	}
-	
+
 	@EventHandler
-	public void onDeleteEvent (IslandDeleteEvent e)
-	{
+	public void onDeleteEvent(IslandDeleteEvent e) {
 		islandDeletion(e.getIsland());
 	}
-	
-	@EventHandler
-	public void onDeleteEvent (IslandRegenEvent e)
-	{
-		islandDeletion(e.getIsland());
-	}
-	
-	private void islandDeletion(Island is)
-	{
-		World[] worlds = {IridiumSkyblockAPI.getInstance().getWorld(), IridiumSkyblockAPI.getInstance().getNetherWorld(), IridiumSkyblockAPI.getInstance().getEndWorld()};
-		
-		for (World w : worlds)
-		{
-			Location min = is.getPos1(w);
-			Location max = is.getPos2(w);
-			
-			Logger.info("Detected IridiumSkyblock removing island in world " + min.getWorld().getName() + " starting at " + min.getBlockX() + "," + min.getBlockZ() + " and ending at " + max.getBlockX()+","+max.getBlockZ());
-			Main.getPlacedGenerators().bulkRemoveGenerators(min.getWorld(), min.getBlockX(), 0, min.getBlockZ(), max.getBlockX(), min.getWorld().getMaxHeight(), max.getBlockZ(), false);
+
+	private void islandDeletion(Island is) {
+		World[] worlds = { IridiumSkyblockAPI.getInstance().getWorld(),
+				IridiumSkyblockAPI.getInstance().getNetherWorld(), IridiumSkyblockAPI.getInstance().getEndWorld() };
+
+		for (World w : worlds) {
+
+			Location min = is.getPosition1(w);
+			Location max = is.getPosition2(w);
+
+			Logger.info("Detected IridiumSkyblock removing island in world " + min.getWorld().getName()
+					+ " starting at " + min.getBlockX() + "," + min.getBlockZ() + " and ending at " + max.getBlockX()
+					+ "," + max.getBlockZ());
+			Main.getPlacedGenerators().bulkRemoveGenerators(min.getWorld(), min.getBlockX(), 0, min.getBlockZ(),
+					max.getBlockX(), min.getWorld().getMaxHeight(), max.getBlockZ(), false);
 		}
 	}
 
